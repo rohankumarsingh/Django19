6708d9648a799fce99889779d8e6e7344105a0cc
rohankumarsingh <rohan24malhotra@gmail.com>
2017-11-01 15:45:33 +0530

    third commit

--- tryjango19/posts/migrations/0001_initial.py
+++ tryjango19/posts/migrations/0001_initial.py
@@ -19,7 +19,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=120)),
                 ('content', models.TextField()),
-                ('update', models.DateTimeField(auto_now=True)),
+                ('updated', models.DateTimeField(auto_now=True)),
                 ('timestamp', models.DateTimeField(auto_now_add=True)),
             ],
         ),
--- tryjango19/posts/models.py
+++ tryjango19/posts/models.py
@@ -1,3 +1,4 @@
+
 from django.db import models
 
 # Create your models here.
@@ -6,7 +7,7 @@ from django.db import models
 class Post(models.Model):
 	title=models.CharField(max_length=120)
 	content=models.TextField()
-	update=models.DateTimeField(auto_now=True,auto_now_add=False)
+	updated=models.DateTimeField(auto_now=True,auto_now_add=False)
 	timestamp=models.DateTimeField(auto_now=False,auto_now_add=True)
 
 	def _unicode_ (self):
