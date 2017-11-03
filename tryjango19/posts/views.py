74630d60dd664e519efb8badb129e3de09f37676
rohankumarsingh <rohan24malhotra@gmail.com>
2017-11-03 06:58:24 +0530

    change done

--- tryjango19/posts/views.py
+++ tryjango19/posts/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from posts.models import Post
+
 def post_create(request):
 	return HttpResponse("<h1>Create</h1>")
 
@@ -14,8 +16,9 @@ def post_detail(request):#reterive
 	#return HttpResponse("<h1>detail</h1>")
 
 def post_list(request):  #list 
-
+    queryset=Post.objects.all()
     context={
+        "object_list":queryset,
     	"title":'List'
     }
    # if request.user.is_authenticated():
