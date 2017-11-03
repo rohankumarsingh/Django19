ea4d76c3389f2d6537b29f9d9eb97794d506ddaf
rohankumarsingh <rohan24malhotra@gmail.com>
2017-11-03 05:56:59 +0530

    change done

--- tryjango19/tryjango19/settings.py
+++ tryjango19/tryjango19/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR=(venv) PS C:\Users\HP-ROHAN\Desktop\Django19\tryjango19>
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'tryjango19.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
